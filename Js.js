3f5377eca18283fb65049e2a8673be25e5928cb5
Jawad-git <mohammed.aljawadd@gmail.com>
2024-08-22 11:06:45 +0300

    Finish the functionality, in future fix names & do some styling

--- Js.js
+++ Js.js
@@ -1,19 +1,103 @@
 
-const DisplayController = (function (){
-    function printboard(gameboard)
-    {
-        let board = document.querySelector(".gameboard");
-        let boardArr = "["
-        gameboard.forEach(element => {
-            boardArr += element + ", ";
-        });
-        boardArr += "]";
-        board.textContent = boardArr;a
-    }
-    return {printboard};
-})();
 let Game = (function ()
 {
+    const displayController = (function (){
+        function printboard(gameboard)
+        {
+            let cells = document.querySelectorAll(".cell");
+            cells.forEach((cell, index) =>
+            {
+                cell.textContent = gameboard[index] || "";
+            })
+        }
+        function printGameScore(returnedGame, player1, player2)
+        {
+            let gameText = document.querySelector(".gameScore");
+            if (returnedGame == 1)
+            {
+                gameText.innerHTML = `Congratulations to ${player1.name} you win!`;
+            }
+            else if (returnedGame == 2)
+            {
+                gameText.innerHTML = `Congratulations to ${player2.name} you win!`;
+            }
+            else if (returnedGame == 0)
+            {
+                gameText.innerHTML = `The game ended with a draw!`;
+            }
+        }
+        function printTotalScore(player1, player2)
+        {
+            let scoreText = document.querySelector(".totalGameScore");
+            scoreText.innerHTML = `The current score is now ${player1.name} ${player1.getScore()} - ${player2.getScore()} ${player2.name}`;
+        }
+        function printTurn(player1, player2, turn)
+        {
+            let turnText = document.querySelector(".turn");
+            if (turn == 0)
+                turnText.innerHTML = `It is now ${player1.name}'s turn`;
+            if (turn == 1)
+                turnText.innerHTML = `It is now ${player2.name}'s turn`;
+        }
+        function clearCells()
+        {
+            let cells = document.querySelectorAll(".cell");
+            cells.forEach((cell) =>
+            {
+                cell.textContent = "";
+            })
+        }
+        return {printboard, printGameScore, printTotalScore, printTurn, clearCells};
+    })();
+    let initialize = function()
+    {
+        let cells = document.querySelectorAll(".cell");
+        cells.forEach((cell) => 
+        {
+            cell.addEventListener("click", cellClickListener);
+        });
+        let playAgain = document.querySelector(".playAgain");
+        playAgain.addEventListener("click", () =>
+        {
+            playOnce();
+        });
+    };
+    let playOnce = () => {
+        round = gameRound(P1, P2, displayController);
+        displayController.clearCells();
+        enableCells(); // Re-enable cells for the new game
+    }
+    let cellClickListener = function (e) {
+        let slot = e.target.dataset.index;
+        let game = round.play(slot);
+        if (game != null) {
+            if (game == 1) {
+                P1.incScore();
+                displayController.printGameScore(1, P1, P2);
+            } else if (game == 2) {
+                P2.incScore();
+                displayController.printGameScore(2, P1, P2);
+            } else if (game == 0) {
+                displayController.printGameScore(0, P1, P2);
+            }
+            displayController.printTotalScore(P1, P2);
+            disableCells(); // Disable cells after game ends
+        }
+    };
+    function disableCells() {
+        let cells = document.querySelectorAll(".cell");
+        cells.forEach((cell) => {
+            cell.removeEventListener("click", cellClickListener);
+        });
+    }
+
+    function enableCells() {
+        let cells = document.querySelectorAll(".cell");
+        cells.forEach((cell) => {
+            cell.addEventListener("click", cellClickListener);
+        });
+    }
+
     let playerName1 = prompt("Name of first player: ");
     let symbol1 = prompt("Symbol of first player: ");
     let playerName2;
@@ -28,27 +112,68 @@ let Game = (function ()
         symbol2 = prompt("Symbol of Second player: ");
     } while (symbol1 == symbol2);
     
-    P1 = newPlayer(playerName1, symbol1);
-    P2 = newPlayer(playerName2, symbol2);
-    const PlayOnce = function()
-    {
-        let round = gameRound(P1, P2);
-        let game = round.play();
-        if (game == 1)
-        {
-            P1.incScore();
-            console.log(P1.getScore());
-        }
-        if (game == 2)
-        {
-            P2.incScore();
-            console.log(P2.getScore());
-        }
-        return game;
-    }
-    return {PlayOnce};
+    const P1 = newPlayer(playerName1, symbol1);
+    const P2 = newPlayer(playerName2, symbol2);
+    let round = gameRound(P1, P2, displayController);
+    return {playOnce, initialize};
 })();
-Game.PlayOnce();
+Game.initialize();
+
+function gameRound (player1, player2, displayController)
+{
+    var turn = Math.round(Math.random());
+    let gameboard = Array(9).fill(null);
+    let counter = 0;
+
+
+
+    function play (slot){
+                if (gameboard[slot] === null)
+                {
+                    counter++;
+                    displayController.printTurn(player1, player2, turn);
+                    if (turn == 0)
+                    {
+                        gameboard[slot] = player1.symbol;
+                        turn = 1;
+                    }
+                    else
+                    {
+                        gameboard[slot] = player2.symbol;
+                        turn = 0;
+                    }
+                    displayController.printTurn(player1, player2, turn);
+                    displayController.printboard(gameboard);
+                if (checkResult() != 0 || counter == 9)
+                {
+                    return checkResult();
+                }
+                }
+        }
+
+
+
+        function checkResult() {
+            const winningCombos = [
+                [0, 1, 2], [3, 4, 5], [6, 7, 8],
+                [0, 3, 6], [1, 4, 7], [2, 5, 8],
+                [0, 4, 8], [2, 4, 6]
+            ];
+    
+            for (const [a, b, c] of winningCombos) {
+                if (gameboard[a] && gameboard[a] === gameboard[b] && gameboard[a] === gameboard[c]) {
+                    return gameboard[a] === player1.symbol ? 1 : 2;
+                }
+            }
+            return gameboard.every(cell => cell !== null) ? 0 : null; // Return 0 for draw if all cells are filled
+        }
+        
+        return {gameboard, play};
+}
+
+
+
+
 function newPlayer(name, symbol)
 {
     this.name = name;
@@ -63,57 +188,4 @@ function newPlayer(name, symbol)
         score++;
     }
     return {name, symbol, getScore, incScore};
-}
-
-function gameRound (player1, player2)
-{
-    var turn = Math.round(Math.random());
-    let gameboard = [];  //[undefined, undefined, undefined undefined];
-    function play (){
-        while (gameboard.length < 9)
-        {
-            let slot = prompt("what position do you want?");
-            if (turn == 0)
-            {
-                gameboard[slot] = player1.symbol;
-                turn = 1;
-            }
-            else
-            {
-                gameboard[slot] = player2.symbol;
-                turn = 0;
-            }
-            if (checkResult() != 0 || gameboard.length == 9)
-            {
-                DisplayController.printboard(gameboard);
-                return checkResult();
-            }
-        }
-        function checkResult() {
-            const winningCombos = [
-                [0, 1, 2],
-                [3, 4, 5],
-                [6, 7, 8],
-                [0, 3, 6],
-                [1, 4, 7],
-                [2, 5, 8],
-                [0, 4, 8],
-                [2, 4, 6]
-            ];
-        
-            for (const combo of winningCombos) {
-                if (gameboard[combo[0]] === player1.symbol && gameboard[combo[1]] === player1.symbol && gameboard[combo[2]] === player1.symbol) {
-                    return 1;
-                }
-                if (gameboard[combo[0]] === player2.symbol && gameboard[combo[1]] === player2.symbol && gameboard[combo[2]] === player2.symbol) {
-                    return 2;
-                }
-            }
-            
-            // If no win condition is met, return 0
-            return 0;
-        }
-
-    }
-    return {gameboard, play};
 }
